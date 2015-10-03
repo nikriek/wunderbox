ae024b07e8ec6eeba296bba301e673d686515bbf
Niklas Riekenbrauck <nikriek@gmail.com>
2015-10-03 21:59:25 +0200

    dropbox login works

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,13 @@
+class Users::OmniauthCallbacksController < ApplicationController
+  def dropbox_oauth2
+    # You need to implement the method below in your model (e.g. app/models/user.rb)
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+
+    if @user.persisted?
+      sign_in_and_redirect @user, :action => :"authentication" #this will throw if @user is not activated
+    else
+      session["devise.dropbox_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
   post '/order', to: 'order#create'
   get '/congratulations',  to: 'order#congratulations'
   get '/order',  to: 'order#new'
