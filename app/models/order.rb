a4dfc5cd4443c8e9d73f637e6e190620c6d6a23a
Niklas Riekenbrauck <nikriek@gmail.com>
2015-10-04 11:07:37 +0200

    Fixied

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,5 @@
 class Order < ActiveRecord::Base
+  require "dropbox_sdk"
   enum category: [:pictures, :literature, :music, :games, :gifs, :stuff]
   enum pricing: [:zero, :one, :two, :five, :ten]
 
@@ -9,11 +10,11 @@ class Order < ActiveRecord::Base
   validates_presence_of :pricing
 
   def process
-    niklas = User.find(:first, :conditions => [ "name = ?", "Niklas Willichnichtangeben"])
-    client = DropboxClient.new(niklas.access_token)
-    client2 = DropboxClient.new(order.user.access_token)
+    niklas = User.first
+    client = DropboxClient.new("xlrrekELL14AAAAAAAAJyrVZyc0cZyuGumWej3AOJYyi20hYNWhSCsKYrv6DBOfS")
+    client2 = DropboxClient.new(self.user.access_token)
 
-    download_path = "/res/" + self.pricing + "/" + self.pictures
+    download_path = "/res/" + self.pricing + "/" + self.category
     download_metadata = client.metadata(download_path)
     downloadable_files = download_metadata["contents"].select {|item| item["is_dir"] == false }  
     downloadable_files = downloadable_files.shuffle
@@ -28,7 +29,7 @@ class Order < ActiveRecord::Base
     for path in download_paths
       contents, metadata = client.get_file_and_metadata(path)
       filename = File.basename(path)
-      open(filename, 'w') {|f| f.puts contents }
+      open(filename, 'wb') {|f| f.puts contents }
       client2.put_file("/" + filename, open(filename))
       File.delete(filename)
     end
